99893aca17849ff76a9f38d15af0c142f0ab8dcf
Mohamed Bihi <mohamedabdi2037@gmail.com>
2025-09-03 21:57:04 +0100

    this is my javascript course for web development bootcamp

---
+++ ch2_variables_datatypes/script.js
@@ -0,0 +1,14 @@
+var firstName = "Mohamad"
+var lastName = "Bihi"
+var age = 24
+var isStudent = true
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isStudent);
