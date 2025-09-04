91934f57567621b96f7d42669b9c99dac17a4b9d
Mohamed Bihi <mohamedabdi2037@gmail.com>
2025-09-04 19:31:14 +0100

    I have added variables and datatypes

--- ch2_variables_datatypes/script.js
+++ ch2_variables_datatypes/script.js
@@ -1,14 +1,16 @@
-var firstName = "Mohamad"
-var lastName = "Bihi"
-var age = 24
-var isStudent = true
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
-console.log(lastName);
-console.log(age);
-console.log(isStudent);
+// var firstName = "Mohamad" //string
+// var lastName = "Bihi"  //string
+// var age = 24 // numbers
+// var isStudent = true //boolean
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age * age);
+// console.log(isStudent);
+
+// var CapitalCity = null
+// // city = "London"
+// // city = "Manchester"
+// console.log(capitalCity);
+
+var theBestThingInTheWorld = 5
+console.log( typeof theBestThingInTheWorld);
